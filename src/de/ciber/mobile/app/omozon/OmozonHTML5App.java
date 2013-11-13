74ac7b6876c949245341cfb6880b5de93cfd5c71
heiko <heiko.jungholt@gmail.com>
2013-11-13 19:39:53 +0100

    recent changes

--- src/de/ciber/mobile/app/omozon/OmozonHTML5App.java
+++ src/de/ciber/mobile/app/omozon/OmozonHTML5App.java
@@ -19,18 +19,64 @@
 
 package de.ciber.mobile.app.omozon;
 
+import java.lang.reflect.Method;
+
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.WebView;
+
 import org.apache.cordova.*;
 
 public class OmozonHTML5App extends DroidGap
 {
-    @Override
+	private int retryCount = 0;
+	
+	@Override
     public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
+//		super.setStringProperty("loadingDialog", "Please wait -- loading...");
+//        super.init();
+		
+		if (android.os.Build.VERSION.SDK_INT > android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+    	    fixJellyBeanIssues();
+    	}
+    	
         // Set by <content src="index.html" /> in config.xml
         super.loadUrl(Config.getStartUrl());
         //super.loadUrl("file:///android_asset/www/index.html")
+    }
+    
+    @TargetApi(16)
+    protected void fixJellyBeanIssues() {    
+        try {
+        	System.out.println(super.appView.toString());
+        	WebView webView = super.appView;
+        	webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+            Class<?> clazz = webView.getSettings().getClass();
+            Method method = clazz.getMethod("setAllowUniversalAccessFromFileURLs", boolean.class);
+    	    if (method != null) {
+    	        method.invoke(webView.getSettings(), true);
+    	    }
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    // catch an error and if try again 1x or quit
+    @Override
+    public void onReceivedError( int errorCode, String description, String failingUrl)
+    {
+        if(retryCount < 3) {
+            retryCount++;
+            System.out.println("Connection failed, trying again. Retry Count: "+retryCount);
+            super.loadUrl("file:///android_asset/www/index.html");
+        } else {
+            System.out.println("Sorry, it failed three times so I give up.");
+            super.loadUrl("file:///android_asset/www/fail.html");
+        }
+        return;
     }
 }
 
